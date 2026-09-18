436982e2ee03479dff4e603dc7292c79eddaaf0c
Oktay Aydoğan <31257020+oktayaydogan@users.noreply.github.com>
2026-09-18 21:32:43 +0300

    feat: expose simulation API

---
+++ packages/simulation/src/index.ts
@@ -0,0 +1,9 @@
+export { DEFAULT_TICK_RATE, Simulation } from "./Simulation";
+export type {
+  GameCommand,
+  MoveCommand,
+  SimulationOptions,
+  SimulationSnapshot,
+  UnitState,
+  Vector2
+} from "./types";
