4124d34447c7b4d18f154cc9e64941a9ae1d486f
Oktay Aydoğan <31257020+oktayaydogan@users.noreply.github.com>
2026-09-18 22:16:08 +0300

    feat: export economy simulation types

--- packages/simulation/src/index.ts
+++ packages/simulation/src/index.ts
@@ -1,12 +1,21 @@
 export { GridNavigation } from "./GridNavigation";
 export { DEFAULT_TICK_RATE, Simulation } from "./Simulation";
 export type {
+  CargoState,
+  DropOffPointState,
   GameCommand,
+  GatherCommand,
   GridCell,
   GridMapDefinition,
   MoveCommand,
+  PlayerStockpileState,
+  ResourceKind,
+  ResourceNodeState,
+  ResourceStockpile,
   SimulationOptions,
   SimulationSnapshot,
+  UnitActivity,
+  UnitKind,
   UnitState,
   Vector2
 } from "./types";
